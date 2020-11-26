2d1625bb058626634fd4f8f4442d8b798c284a15
Filipe Veronezi <filipeseidi@hotmail.com>
2020-11-26 01:10:07 -0300

    Adding sequelize create and migrate cli commands to docker-compose to be executed when db does not exist.

--- backend/wait-for-postgres.sh
+++ backend/wait-for-postgres.sh
@@ -9,10 +9,24 @@ host="$1"
 shift
 cmd="$@"
   
-until PGPASSWORD=docker psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=docker psql -h "$host" -U "postgres" -c '\q' > /dev/null 2>&1; do
   >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
+  sleep 5
 done
-  
->&2 echo "Postgres is up - executing command"
+
+echo "Postgres avaiable!"
+
+RESULT=$(PGPASSWORD=docker psql -h "$host" -U "postgres" -c 'select 1' -d societysql || echo "0")
+
+if [ $RESULT = "0" > /dev/null 2>&1 ];
+then
+  echo "Creating database..."
+  npx sequelize db:create
+  echo "Migrating database..."
+  npx sequelize db:migrate
+  >&2 echo "Postgres is ready!"
+else
+  >&2 echo "Database already exists. Postgres is ready!"
+fi
+
 exec $cmd
